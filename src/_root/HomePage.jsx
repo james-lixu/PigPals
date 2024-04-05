9077341f3437d637f26a99441ca8c3d6ff7058a3
James Li-Xu <36280940+james-lixu@users.noreply.github.com>
2024-04-04 23:29:14 -0400

    homepage

--- src/_root/HomePage.jsx
+++ src/_root/HomePage.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import NavBar from "../components/Navbar.jsx";
+import LeftPig from "../assets/left-pig.jpg";
+import RightPig from "../assets/right-pig.jpg";
 
 const HomePage = () => {
   return (
-    <div>
+    <div className="flex-l w-full">
       <NavBar></NavBar>
+      <div className="flex max-w-100">
+        <img src={LeftPig}></img>
+        <img src={RightPig}></img>
+      </div>
     </div>
   );
 };
